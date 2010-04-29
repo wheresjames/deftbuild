8da9d202c8bbdd5fc35f93ba05a0821508e24ff2
ru <ru@wheresjames.com>
2010-04-29 17:04:23 -0400

    + Adding tiff config

--- v1/dep/lib_tiff.mk
+++ v1/dep/lib_tiff.mk
@@ -25,7 +25,14 @@ export LOC_TAG := def
 LOC_CXX_def := c
 LOC_INC_def := $(CFG_LIBROOT)/tiff/libtiff
 LOC_SRC_def := $(CFG_LIBROOT)/tiff/libtiff
-LOC_EXC_def := tif_vms tif_wince tif_unix
+LOC_EXC_def := 
+
+ifeq ($(PLATFORM),windows)
+	LOC_EXC_def := $(LOC_EXC_def) tif_vms tif_wince tif_unix
+else
+	LOC_EXC_def := $(LOC_EXC_def) tif_vms tif_wince tif_win32
+endif
+
 include $(PRJ_LIBROOT)/build.mk
 
 #-------------------------------------------------------------------
