51d2254d97f84b998c9f1ac93ca18c8387250d85
ru <ru@wheresjames.com>
2010-05-12 14:40:32 -0400

    + Windows fixes for portaudio build script

--- v1/dep/lib_portaudio.mk
+++ v1/dep/lib_portaudio.mk
@@ -28,15 +28,41 @@ LOC_SRC_def := $(CFG_LIBROOT)/portaudio/src/common
 include $(PRJ_LIBROOT)/build.mk
 
 ifeq ($(PLATFORM),windows)
+
+PRJ_DEFS := $(PRJ_DEFS) PA_NO_ASIO PA_NO_WASAPI
+PRJ_INCS := $(PRJ_INCS) portaudio/src/os/win
+
 export LOC_TAG := win
 LOC_CXX_win := c
 LOC_SRC_win := $(CFG_LIBROOT)/portaudio/src/os/win
 include $(PRJ_LIBROOT)/build.mk
+
+# export LOC_TAG := asio
+# LOC_SRC_asio := $(CFG_LIBROOT)/portaudio/src/hostapi/asio
+# include $(PRJ_LIBROOT)/build.mk
+
+export LOC_TAG := dsound
+LOC_CXX_dsound := c
+LOC_SRC_dsound := $(CFG_LIBROOT)/portaudio/src/hostapi/dsound
+include $(PRJ_LIBROOT)/build.mk
+
+# export LOC_TAG := wasapi
+# LOC_CXX_wasapi := c
+# LOC_SRC_wasapi := $(CFG_LIBROOT)/portaudio/src/hostapi/wasapi
+# include $(PRJ_LIBROOT)/build.mk
+
+export LOC_TAG := wmme
+LOC_CXX_wmme := c
+LOC_SRC_wmme := $(CFG_LIBROOT)/portaudio/src/hostapi/wmme
+include $(PRJ_LIBROOT)/build.mk
+
 else
+
 export LOC_TAG := unix
 LOC_CXX_unix := c
 LOC_SRC_unix := $(CFG_LIBROOT)/portaudio/src/os/unix
 include $(PRJ_LIBROOT)/build.mk
+
 endif
 
 
