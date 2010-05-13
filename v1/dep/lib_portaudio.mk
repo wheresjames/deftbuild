cd866bcd0a6572551f9a42783e9dda0798ae57c6
ru <ru@wheresjames.com>
2010-05-13 14:50:31 -0400

    + portaudio fixes for Linux

--- v1/dep/lib_portaudio.mk
+++ v1/dep/lib_portaudio.mk
@@ -19,6 +19,19 @@ PRJ_OBJROOT := _0_dep
 #-------------------------------------------------------------------
 include $(PRJ_LIBROOT)/config.mk
 
+ifeq ($(PLATFORM),windows)
+	PRJ_DEFS := $(PRJ_DEFS) PA_NO_ASIO PA_NO_WASAPI
+	PRJ_INCS := $(PRJ_INCS) portaudio/src/os/win
+ifneq ($(BUILD),vs)
+	PRJ_DEFS := $(PRJ_DEFS) PA_NO_DS
+endif
+
+else
+	PRJ_DEFS := $(PRJ_DEFS) PA_USE_ALSA
+	PRJ_INCS := $(PRJ_INCS) portaudio/src/os/unix
+endif
+
+
 #-------------------------------------------------------------------
 # File locations
 #-------------------------------------------------------------------
@@ -29,22 +42,24 @@ include $(PRJ_LIBROOT)/build.mk
 
 ifeq ($(PLATFORM),windows)
 
-PRJ_DEFS := $(PRJ_DEFS) PA_NO_ASIO PA_NO_WASAPI
-PRJ_INCS := $(PRJ_INCS) portaudio/src/os/win
-
 export LOC_TAG := win
 LOC_CXX_win := c
 LOC_SRC_win := $(CFG_LIBROOT)/portaudio/src/os/win
+LOC_EXC_win := pa_win_wdmks_utils
 include $(PRJ_LIBROOT)/build.mk
 
 # export LOC_TAG := asio
 # LOC_SRC_asio := $(CFG_LIBROOT)/portaudio/src/hostapi/asio
 # include $(PRJ_LIBROOT)/build.mk
 
+ifeq ($(BUILD),vs)
+
 export LOC_TAG := dsound
 LOC_CXX_dsound := c
 LOC_SRC_dsound := $(CFG_LIBROOT)/portaudio/src/hostapi/dsound
 include $(PRJ_LIBROOT)/build.mk
+
+endif
 
 # export LOC_TAG := wasapi
 # LOC_CXX_wasapi := c
@@ -62,6 +77,21 @@ export LOC_TAG := unix
 LOC_CXX_unix := c
 LOC_SRC_unix := $(CFG_LIBROOT)/portaudio/src/os/unix
 include $(PRJ_LIBROOT)/build.mk
+
+export LOC_TAG := alsa
+LOC_CXX_alsa := c
+LOC_SRC_alsa := $(CFG_LIBROOT)/portaudio/src/hostapi/alsa
+include $(PRJ_LIBROOT)/build.mk
+
+# export LOC_TAG := jack
+# LOC_CXX_jack := c
+# LOC_SRC_jack := $(CFG_LIBROOT)/portaudio/src/hostapi/jack
+# include $(PRJ_LIBROOT)/build.mk
+
+# export LOC_TAG := oss
+# LOC_CXX_oss := c
+# LOC_SRC_oss := $(CFG_LIBROOT)/portaudio/src/hostapi/oss
+# include $(PRJ_LIBROOT)/build.mk
 
 endif
 
