da418abecd488effc3d3e3ea76105177c341b648
ru <ru@wheresjames.com>
2010-05-24 15:28:30 -0400

    + exists check on app projects

--- v1/app/lib_skwikin.mk
+++ v1/app/lib_skwikin.mk
@@ -5,9 +5,15 @@ PRJ_LIBROOT := ..
 PRJ_DEPS := skwikin
 include $(PRJ_LIBROOT)/config.mk
 
+ifeq ($(strip $(EXISTS_LIBSRC)),) 
+UNSUPPORTED := $(PRJ_DEPS) is not checked out
+include $(PRJ_LIBROOT)/unsupported.mk
+else
+
 #-------------------------------------------------------------------
 # skwikin is native
 #-------------------------------------------------------------------
 all:
 	$(MAKE) -C $(CFG_LIBROOT)/skwikin
 
+endif
--- v1/app/lib_winglib.mk
+++ v1/app/lib_winglib.mk
@@ -5,9 +5,15 @@ PRJ_LIBROOT := ..
 PRJ_DEPS := winglib
 include $(PRJ_LIBROOT)/config.mk
 
+ifeq ($(strip $(EXISTS_LIBSRC)),) 
+UNSUPPORTED := $(PRJ_DEPS) is not checked out
+include $(PRJ_LIBROOT)/unsupported.mk
+else
+
 #-------------------------------------------------------------------
 # winglib is native
 #-------------------------------------------------------------------
 all:
 	$(MAKE) -C $(CFG_LIBROOT)/winglib
 
+endif
