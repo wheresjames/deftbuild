8efaec3ff3af21e71ebc73313749208dfbe68c3f
ru <ru@wheresjames.com>
2010-09-03 18:15:39 -0400

    + Added libusb

---
+++ v1/dep/lib_libusb-win32.mk
@@ -0,0 +1,47 @@
+
+default_target: all
+
+#-------------------------------------------------------------------
+# Project
+#-------------------------------------------------------------------
+PRJ_NAME := usb-win32
+PRJ_DEPS := libusb-win32
+PRJ_TYPE := lib
+PRJ_INCS := libusb-win32/libusb libusb-win32/msvc
+PRJ_LIBS := 
+
+PRJ_LIBROOT := ..
+PRJ_OBJROOT := _0_dep
+
+#-------------------------------------------------------------------
+# Configure build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/config.mk
+
+ifeq ($(PLATFORM),windows)
+	PRJ_INCS := $(PRJ_INCS) libusb-win32/libusb libusb-win32/msvc
+else
+	PRJ_INCS := $(PRJ_INCS) libusb/libusb
+endif
+
+#-------------------------------------------------------------------
+# File locations
+#-------------------------------------------------------------------
+export LOC_TAG := def
+LOC_CXX_def := c
+LOC_SRC_def := $(CFG_LIBROOT)/libusb-win32/libusb
+LOC_EXC_def :=
+include $(PRJ_LIBROOT)/build.mk
+
+export LOC_TAG := os
+LOC_CXX_os := c
+LOC_SRC_os := $(CFG_LIBROOT)/libusb-win32/libusb/os
+LOC_EXC_os := darwin_usb linux_usbfs
+include $(PRJ_LIBROOT)/build.mk
+
+
+#-------------------------------------------------------------------
+# Execute the build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/go.mk
+
