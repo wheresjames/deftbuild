dec588ddbc5179045d6fde5cb8ee21fb76d195b6
ru <ru@wheresjames.com>
2010-12-22 18:39:52 -0500

    + Adding libssh2

---
+++ v1/dep/lib_libssh2.mk
@@ -0,0 +1,39 @@
+
+default_target: all
+
+#-------------------------------------------------------------------
+# Project
+#-------------------------------------------------------------------
+PRJ_NAME := ssh2
+PRJ_DEPS := libssh2
+PRJ_TYPE := lib
+PRJ_INCS := libssh2/include openssl/include zlib
+PRJ_LIBS := 
+PRJ_DEFS := USE_OPENSSL USE_SSLEAY
+
+PRJ_LIBROOT := ..
+PRJ_OBJROOT := _0_dep
+
+#-------------------------------------------------------------------
+# Configure build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/config.mk
+
+ifeq ($(PLATFORM),windows)
+	PRJ_INCS := $(PRJ_INCS) libssh2/win32
+endif
+
+#-------------------------------------------------------------------
+# File locations
+#-------------------------------------------------------------------
+export LOC_TAG := def
+LOC_CXX_def := c
+LOC_SRC_def := $(CFG_LIBROOT)/libssh2/src
+LOC_EXC_def :=  
+include $(PRJ_LIBROOT)/build.mk
+
+#-------------------------------------------------------------------
+# Execute the build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/go.mk
+
