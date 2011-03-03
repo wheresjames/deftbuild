c682752f8abe706d3f91b260c777b4add1660f52
ru <ru@wheresjames.com>
2011-03-03 11:46:42 -0500

    + Added build script for cvblobslib

---
+++ v1/dep/lib_cvblobslib.mk
@@ -0,0 +1,33 @@
+
+default_target: all
+
+#-------------------------------------------------------------------
+# Project
+#-------------------------------------------------------------------
+PRJ_NAME := cvblobslib
+PRJ_DEPS := cvblobslib
+PRJ_TYPE := lib
+PRJ_INCS := opencv/include/opencv
+PRJ_LIBS := 
+
+PRJ_LIBROOT := ..
+PRJ_OBJROOT := _0_dep
+
+#-------------------------------------------------------------------
+# Configure build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/config.mk
+
+#-------------------------------------------------------------------
+# File locations
+#-------------------------------------------------------------------
+export LOC_TAG := def
+LOC_SRC_def := $(CFG_LIBROOT)/cvblobslib
+LOC_EXC_def := BlobProperties
+include $(PRJ_LIBROOT)/build.mk
+
+#-------------------------------------------------------------------
+# Execute the build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/go.mk
+
