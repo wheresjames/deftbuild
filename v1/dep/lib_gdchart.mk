2ba4fbb0d662104024be6b7991263c16d39a52ae
ru <ru@wheresjames.com>
2011-08-04 10:21:30 -0400

    + Removing legacy stuff

--- v1/dep/lib_gdchart.mk
+++ v1/dep/lib_gdchart.mk
@@ -30,7 +30,6 @@ else
 export LOC_TAG := def
 LOC_CXX_def := c
 LOC_BLD_def := cpp
-LOC_INC_def := $(CFG_LIBROOT)/gdchart
 LOC_SRC_def := $(CFG_LIBROOT)/gdchart
 LOC_EXC_def := gdc_samp1 gdc_samp2 gdc_pie_samp
 include $(PRJ_LIBROOT)/build.mk
@@ -38,7 +37,6 @@ include $(PRJ_LIBROOT)/build.mk
 export LOC_TAG := gd
 LOC_CXX_gd := c
 LOC_BLD_gd := cpp
-LOC_INC_gd := $(CFG_LIBROOT)/gdchart/gd1.3
 LOC_SRC_gd := $(CFG_LIBROOT)/gdchart/gd1.3
 LOC_EXC_gd := gddemo giftogd webgif mathmake mtables
 include $(PRJ_LIBROOT)/build.mk
