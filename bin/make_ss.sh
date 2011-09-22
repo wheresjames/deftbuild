3d6408ea02b452bbf1c5a10243dc1ae2f4255072
ru <ru@wheresjames.com>
2011-09-22 12:05:12 -0400

    + Adding a script to generate self signed certificate

---
+++ bin/make_ss.sh
@@ -0,0 +1,7 @@
+
+# makes a self signed certificate
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $1.pem -out $1.pem
+openssl openssl pkcs12 -export -out $1.pfx -in $1.pem -name “$1”
+openssl pkcs12 -in $1.pfx -out $1.pem
+openssl pkcs12 -export -in $1.pem -out $1.ks -name "$1"
