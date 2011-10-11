c31253f8a33b50218b0e76d392bda0c1f979d319
ru <ru@wheresjames.com>
2011-10-11 15:08:58 -0400

    + Add public key export

--- bat/make_ss.bat
+++ bat/make_ss.bat
@@ -1,12 +1,12 @@
 
 REM makes a self signed certificate
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout %1.pem -out %1.pem
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout %1.pem -out %1.pem -passin pass:"%2"
 
-openssl pkcs12 -export -out %1.pfx -in %1.pem -name "%1"
+openssl pkcs12 -export -out %1.pfx -in %1.pem -name "%1" -passin pass:"%2" -passout pass:"%2"
 
-openssl pkcs12 -in %1.pfx -out %1.pem
+openssl pkcs12 -in %1.pfx -out %1.pem -passin pass:"%2" -passout pass:"%2"
 
-openssl pkcs12 -export -in %1.pem -out %1.ks -name "%1"
+openssl pkcs12 -export -in %1.pem -out %1.ks -name "%1" -passin pass:"%2" -passout pass:"%2"
 
-openssl pkcs12 -in %1.pfx -out %1.pub -clcerts -nokeys
+openssl rsa -in %1.pem -out %1.pub -pubout -passin pass:"%2"
