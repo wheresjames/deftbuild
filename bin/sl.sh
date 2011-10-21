33b6f62cddeb6ef5d6449a2fb0abba5db5d1b240
ru <ru@wheresjames.com>
2011-10-21 18:00:53 -0400

    + Changing git log script

--- bin/sl.sh
+++ bin/sl.sh
@@ -1,2 +1,2 @@
-git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative
+git log --graph --pretty=format:'%Cred%h%Creset:%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative
 
