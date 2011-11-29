96d71600133e452eb361daf6b1badddc49b6a994
ru <ru@wheresjames.com>
2011-11-29 12:34:51 -0500

    + yasm missing vpperm

--- v1/dep/lib_x264.mk
+++ v1/dep/lib_x264.mk
@@ -73,6 +73,9 @@ endif
 LOC_SRC_common_asm := $(CFG_LIBROOT)/x264/common/x86
 ifeq ($(PROC),x64)
 	LOC_EXC_common_asm := dct-32 pixel-32
+	ifdef NOVPPERM
+		LOC_EXC_common_asm := $(LOC_EXC_common_asm) dct-a mc-a2
+	endif
 else
 	LOC_EXC_common_asm := dct-64 pixel-64
 endif
