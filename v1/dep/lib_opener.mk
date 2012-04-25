41f0c0cbf3f793df113dafa78cd4638001865a58
ru <ru@wheresjames.com>
2012-04-25 17:10:59 -0400

    + Update dep script

--- v1/dep/lib_opener.mk
+++ v1/dep/lib_opener.mk
@@ -7,7 +7,8 @@ default_target: all
 PRJ_NAME := opener
 PRJ_DEPS := opener
 PRJ_TYPE := lib
-PRJ_INCS := opener/src opener/src/enet_encap opener/src/ports/platform-pc
+PRJ_INCS := opener/src opener/src/cip opener/src/enet_encap \
+			opener/src/ports/platform-pc
 PRJ_LIBS := 
 
 PRJ_LIBROOT := ..
@@ -17,6 +18,13 @@ PRJ_OBJROOT := _0_dep
 # Configure build
 #-------------------------------------------------------------------
 include $(PRJ_LIBROOT)/config.mk
+
+ifeq ($(PLATFORM),windows)
+	ifeq ($(BUILD),vs)
+		PRJ_INCS := $(PRJ_INCS) $(CFG_LIB2BLD)/dep/etc/vs/inc/c99
+	endif
+endif
+
 
 #-------------------------------------------------------------------
 # File locations
@@ -29,6 +37,11 @@ include $(PRJ_LIBROOT)/build.mk
 export LOC_TAG := cip
 LOC_CXX_cip := c
 LOC_SRC_cip := $(CFG_LIBROOT)/opener/src/cip
+include $(PRJ_LIBROOT)/build.mk
+
+export LOC_TAG := pc
+LOC_CXX_pc := c
+LOC_SRC_pc := $(CFG_LIBROOT)/opener/src/ports/platform-pc
 include $(PRJ_LIBROOT)/build.mk
 
 #-------------------------------------------------------------------
