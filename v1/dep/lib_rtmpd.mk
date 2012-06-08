7047057221849cc558cd814b01b1b1050e3e8e5c
ru <ru@wheresjames.com>
2012-06-08 17:42:36 -0400

    + Adding repo for rtmpd

---
+++ v1/dep/lib_rtmpd.mk
@@ -0,0 +1,40 @@
+
+default_target: all
+
+#-------------------------------------------------------------------
+# Project
+#-------------------------------------------------------------------
+PRJ_NAME := rtmpd
+PRJ_DEPS := rtmpd
+PRJ_TYPE := lib
+PRJ_INCS := openssl/include zlib
+PRJ_LIBS := 
+
+PRJ_LIBROOT := ..
+PRJ_OBJROOT := _0_dep
+
+#-------------------------------------------------------------------
+# Configure build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/config.mk
+
+ifeq ($(PLATFORM),windows)
+	ifeq ($(BUILD),vs)
+		PRJ_INCS := $(PRJ_INCS) $(CFG_LIB2BLD)/dep/etc/vs/inc/c99
+	endif
+endif
+
+
+#-------------------------------------------------------------------
+# File locations
+#-------------------------------------------------------------------
+export LOC_TAG := def
+LOC_CXX_def := c
+LOC_SRC_def := $(CFG_LIBROOT)/rtmpd/librtmp
+include $(PRJ_LIBROOT)/build.mk
+
+#-------------------------------------------------------------------
+# Execute the build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/go.mk
+
