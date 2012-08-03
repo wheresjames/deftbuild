a4f14c29950261eead04c2114782d5c379e594e9
ru <ru@wheresjames.com>
2012-08-03 16:57:44 -0400

    + Adding lame repo

---
+++ v1/dep/lib_lame.mk
@@ -0,0 +1,54 @@
+
+default_target: all
+
+#-------------------------------------------------------------------
+# Project
+#-------------------------------------------------------------------
+PRJ_NAME := lame
+PRJ_DEPS := lame
+PRJ_TYPE := lib
+PRJ_INCS := lame/include
+PRJ_LIBS := 
+PRJ_DEFS := 
+
+PRJ_LIBROOT := ..
+PRJ_OBJROOT := _0_dep
+
+#-------------------------------------------------------------------
+# Configure build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/config.mk
+
+ifeq ($(PROC),arm)
+UNSUPPORTED := PROC=$(PROC) is not supported
+include $(PRJ_LIBROOT)/unsupported.mk
+else
+
+ifneq ($(BUILD),gcc)
+UNSUPPORTED := BUILD=$(BUILD) is invalid, can only be built with 'gcc'
+include $(PRJ_LIBROOT)/unsupported.mk
+else
+
+CFG_CFLAGS := $(CFG_CFLAGS) -ffast-math -fomit-frame-pointer -std=c99
+ifdef DBG
+	CFG_CFLAGS := $(CFG_CFLAGS) -fno-stack-check -O1
+endif
+
+#-------------------------------------------------------------------
+# File locations
+#-------------------------------------------------------------------
+
+export LOC_TAG := common
+LOC_CXX_common := c
+LOC_SRC_common := $(CFG_LIBROOT)/lame/libmp3lame
+LOC_EXC_common := 
+include $(PRJ_LIBROOT)/build.mk
+
+#-------------------------------------------------------------------
+# Execute the build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/go.mk
+
+endif
+
+endif
