ad54f067dae609a1d7c38030583c8c5d39d35586
ru <ru@wheresjames.com>
2015-09-02 13:36:14 -0400

    + New files

---
+++ v1/app/lib_peercon_server.mk
@@ -0,0 +1,52 @@
+
+default_target: all
+
+#-------------------------------------------------------------------
+# Project
+#-------------------------------------------------------------------
+PRJ_NAME := peercon-server
+PRJ_DEPS := peercon
+PRJ_TYPE := exe
+PRJ_INCS := webrtc jsoncpp/include
+PRJ_LIBS := webrtc-talk webrtc-modules-video webrtc-modules-audio webrtc yuv srtp \
+			openssl_a openssl_b openssl_c openssl_a openssl_b openssl_c vpx jsoncpp
+PRJ_DEFS := NOMINMAX _WINSOCKAPI_ WEBRTC_WIN \
+			LIBPEERCONNECTION_LIB \
+			HAVE_SRTP SRTP_RELATIVE_PATH HAVE_X86 \
+			HAVE_WEBRTC_VOICE HAVE_WEBRTC_VIDEO \
+			SSL_USE_OPENSSL HAVE_OPENSSL_SSL_H FEATURE_ENABLE_SSL \
+			WEBRTC_NS_FIXED
+PRJ_WINL := winmm strmiids secur32 dmoguids wmcodecdspuuid msdmo crypt32 \
+			uxtheme winspool windowscodecs
+PRJ_FWRK := mfc
+
+PRJ_LIBROOT := ..
+PRJ_OBJROOT := _0_dep
+
+#-------------------------------------------------------------------
+# Configure build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/config.mk
+
+ifneq ($(PLATFORM),_dont_build_)
+UNSUPPORTED := PLATFORM=$(PLATFORM) is invalid, $(PRJ_NAME) can only be built on Windows
+include $(PRJ_LIBROOT)/unsupported.mk
+else
+
+#-------------------------------------------------------------------
+# File locations
+#-------------------------------------------------------------------
+
+export LOC_TAG := def
+LOC_CXX_def := cc
+LOC_BLD_def := cpp
+LOC_SRC_def := $(CFG_LIBROOT)/peercon/peerconnection/server
+include $(PRJ_LIBROOT)/build.mk
+
+#-------------------------------------------------------------------
+# Execute the build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/go.mk
+
+endif
+
---
+++ v1/app/lib_webrtc_client.mk
@@ -0,0 +1,54 @@
+
+default_target: all
+
+#-------------------------------------------------------------------
+# Project
+#-------------------------------------------------------------------
+PRJ_NAME := webrtc-client
+PRJ_DEPS := webrtc
+PRJ_TYPE := exe
+PRJ_INCS := webrtc jsoncpp/include
+PRJ_LIBS := webrtc-talk webrtc-modules-video webrtc-modules-audio webrtc yuv srtp \
+			openssl_a openssl_b openssl_c openssl_a openssl_b openssl_c vpx jsoncpp
+PRJ_DEFS := NOMINMAX _WINSOCKAPI_ WEBRTC_WIN \
+			LIBPEERCONNECTION_LIB \
+			HAVE_SRTP SRTP_RELATIVE_PATH HAVE_X86 \
+			HAVE_WEBRTC_VOICE HAVE_WEBRTC_VIDEO \
+			SSL_USE_OPENSSL HAVE_OPENSSL_SSL_H FEATURE_ENABLE_SSL \
+			WEBRTC_NS_FIXED
+PRJ_WINL := winmm strmiids secur32 dmoguids wmcodecdspuuid msdmo crypt32 \
+			uxtheme winspool windowscodecs
+PRJ_FWRK := mfc
+
+PRJ_LIBROOT := ..
+PRJ_OBJROOT := _0_dep
+
+#-------------------------------------------------------------------
+# Configure build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/config.mk
+
+ifneq ($(PLATFORM),_dont_build_)
+UNSUPPORTED := PLATFORM=$(PLATFORM) is invalid, $(PRJ_NAME) can only be built on Windows
+include $(PRJ_LIBROOT)/unsupported.mk
+else
+
+CFG_LFLAGS := $(CFG_LFLAGS) /SUBSYSTEM:WINDOWS
+
+#-------------------------------------------------------------------
+# File locations
+#-------------------------------------------------------------------
+
+export LOC_TAG := def
+LOC_CXX_def := cc
+LOC_BLD_def := cpp
+LOC_SRC_def := $(CFG_LIBROOT)/webrtc/talk/examples/peerconnection/client
+include $(PRJ_LIBROOT)/build.mk
+
+#-------------------------------------------------------------------
+# Execute the build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/go.mk
+
+endif
+
